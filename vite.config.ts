c699abf4bb2534757396a447ef7b1cacf4d9bfbc
sammy-nduba <ndubasamuelmwangi@gmail.com>
2025-06-29 14:21:24 +0300

    netlify

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   base: './', 
   plugins: [react()],
-  // optimizeDeps: {
-  //   exclude: ["bippy/dist/jsx-dev-runtime", "bippy/dist/jsx-runtime"],
-  // },
+  optimizeDeps: {
+    exclude: ["bippy/dist/jsx-dev-runtime", "bippy/dist/jsx-runtime"],
+  },
 });
