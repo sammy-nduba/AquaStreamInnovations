cde603f11d600af29e49ef54ced2a2410b26b071
sammy-nduba <ndubasamuelmwangi@gmail.com>
2025-06-29 13:59:07 +0300

    vite

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: './', 
   plugins: [react()],
   optimizeDeps: {
     exclude: ["bippy/dist/jsx-dev-runtime", "bippy/dist/jsx-runtime"],
